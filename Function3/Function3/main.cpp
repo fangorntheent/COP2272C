7bdc4fc44360745d689784b5156b8ba82fcf4604
Gus Lipkin <gus.lipkin@gmail.com>
2019-09-24 09:46:00 -0400

    initial

---
+++ Function3/Function3/main.cpp
@@ -0,0 +1,14 @@
+//Gus Lipkin
+//COP 2272C 01
+//Computer Programming 1
+//Function3
+//September 24, 2019
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    //system("pause");
+    return 0;
+}
