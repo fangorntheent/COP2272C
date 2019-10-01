d03800b5a3ed5ff6f0ace6285e697c6df8134b9d
Gus Lipkin <gus.lipkin@gmail.com>
2019-09-30 21:25:59 -0400

    Fixed for turn in

--- Function3LipkinG/Function3LipkinG/main.cpp
+++ Function3LipkinG/Function3LipkinG/main.cpp
@@ -7,14 +7,21 @@
 #include <iostream>
 using namespace std;
 
-void rShift(int &a, int &b, int &c, int &count) {
+void rShift(int (&array)[3], int &count, int &small, int &big, int &prod) {
     int temp;
     for (int i = 0; i < count; i++) {
-        temp = c;
-        c = b;
-        b = a;
-        a = temp;
+        temp = array[2];
+        array[2] = array[1];
+        array[1] = array[0];
+        array[0] = temp;
     }
+    for (int i = 0; i < 3; i++) {
+        if (i == 0 || array[i] > big)
+            big = array[i];
+        if (i == 0 || array[i] < small)
+            small = array[i];
+    }
+    prod = array[0] * array[1] * array[2];
 }
 
 int main() {
@@ -25,18 +32,10 @@ int main() {
         cin >> num[i];
     cin >> round;
     
-    rShift(num[0], num[1], num[2], round);
+    rShift(num, round, min, max, product);
     
     cout << endl << "The new values for the numbers are: " << num[0] << ", " <<
         num[1] << ", and " << num[2] << ", " << endl;
-    
-    for (int i = 0; i < 3; i++) {
-        if (i == 0 || num[i] > max)
-            max = num[i];
-        if (i == 0 || num[i] < min)
-            min = num[i];
-    }
-    product = num[0] * num[1] * num[2];
     
     cout << endl << "The maximum value is " << max <<
         ". The minimum value is " << min <<
