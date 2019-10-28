64490d57540c96d3aa23b048ad3dec4f43880600
Gus Lipkin <gus.lipkin@gmail.com>
2019-10-28 16:48:44 -0400

    initial

---
+++ OOP3LipkinG/OOP3LipkinG/Sunflower.cpp
@@ -0,0 +1,9 @@
+//
+//  Sunflower.cpp
+//  OOP3LipkinG
+//
+//  Created by Gus Lipkin on 10/28/19.
+//  Copyright © 2019 Gus Lipkin. All rights reserved.
+//
+
+#include "Sunflower.h"
---
+++ OOP3LipkinG/OOP3LipkinG/Sunflower.h
@@ -0,0 +1,15 @@
+//
+//  Sunflower.hpp
+//  OOP3LipkinG
+//
+//  Created by Gus Lipkin on 10/28/19.
+//  Copyright © 2019 Gus Lipkin. All rights reserved.
+//
+
+#define Sunflower_h
+
+class Sunflower {
+private:
+    int numPetal;
+    double stemLength;
+}
