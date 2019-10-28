90f8b4e812631009afdf78919f5abf5b713babe2
Gus Lipkin <gus.lipkin@gmail.com>
2019-10-28 18:57:13 -0400

    Ready for submission

--- OOP3LipkinG/OOP3LipkinG/TestSunflower.cpp
+++ OOP3LipkinG/OOP3LipkinG/TestSunflower.cpp
@@ -19,7 +19,26 @@ int main() {
     cin >> height;
     Sunflower user = Sunflower(petal, height);
     
+    Sunflower sunArray[2] = {sunny, user};
     
+    cout << endl << "We will now discuss the sunflowers in your garden." << endl << endl;
+    
+    for(int i = 0; i < 2; i++) {
+        cout << "This sunflower currently has " << sunArray[i].getNumPetal() <<
+            " petals and is " << sunArray[i].getStemLength() << " feet high." << endl;
+        cout << "Please enter a new petal count for this sunflower: ";
+        cin >> petal;
+        cout << "Please enter a new height for this sunflower in feet: ";
+        cin >> height;
+        
+        sunArray[i].setNumPetal(petal);
+        sunArray[i].setStemLength(height);
+        
+        sunArray[i].display();
+        sunArray[i].grow();
+        
+        cout << endl;
+    }
     
     //system("pause");
     return 0;
